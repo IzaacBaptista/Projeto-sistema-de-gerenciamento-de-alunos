18a69a6197c4e9824d549efd9737d7193df19942
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-08 12:48:50 -0300

    config controllers

--- sms/database/migrations/2022_05_08_151441_create_students_table.php
+++ sms/database/migrations/2022_05_08_151441_create_students_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('cne');
+            $table->string('firstName');
+            $table->string('secondName');
+            $table->integer('age');
+            $table->string('speciality');
             $table->timestamps();
         });
     }
