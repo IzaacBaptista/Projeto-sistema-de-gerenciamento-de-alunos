596c4ba0e6686f2af2c305c0a4169b699ad2d031
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-09 19:53:06 -0300

    views stylization

--- sms/resources/views/student.blade.php
+++ sms/resources/views/student.blade.php
@@ -7,13 +7,16 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <title>Student management system</title>
   </head>
   <body>
     @include("navbar")
     <div class="row header-container justify-content-center">
-        
+        <div class="header">
+            <h1>Student Management System</h1>
+        </div>
     </div>
     @if($layout == 'index')
         <div class="container-fluid mt-4">
@@ -120,6 +123,10 @@
         </div>
 
     @endif
+
+    <footer>
+
+    </footer>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
